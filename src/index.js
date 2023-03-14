0308a1562ba85837ea38384bb5a796237b60a32c
JustWaveThings <justwavethings@proton.me>
2023-03-14 14:10:45 -0400

    Project start with Webpack Template and a form I created earlier.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,63 @@
 import './style.css';
+
+const body = document.querySelector('body');
+
+
+const formContainer = document.querySelector('.form-container');
+
+const form = document.createElement('form');
+form.id = 'book-add';
+form.name = 'book-add';
+
+body.appendChild(form);
+
+const fieldset = document.createElement('fieldset');
+fieldset.id = 'book-add-fieldset';
+fieldset.name = 'book-add-fieldset';
+
+const legend = document.createElement('legend');
+legend.textContent = 'Add New Book';
+fieldset.appendChild(legend);
+
+// Section generator function
+function createBookSection(labelName, inputType, inputId, inputPlaceholder, inputRequired = true ) {
+	const section = document.createElement('div');
+	section.classList.add('form-control');
+	fieldset.appendChild(section);
+
+	const label = document.createElement('label');
+	label.textContent = labelName;
+	label.setAttribute('for', inputId);
+	section.appendChild(label);
+
+	const input = document.createElement(inputType);
+	input.id = inputId;
+	input.name = inputId;
+	input.placeholder = inputPlaceholder;
+	input.required = inputRequired;
+	if (inputId === 'number') {
+		input.min = 1;
+		input.max = 5000;
+		input.type = 'number';
+	}
+	if (inputId === 'title' || inputId === 'author') {
+		input.type = 'text';
+		input.minLength = 2;
+		input.maxLength = 40;
+	}
+	section.appendChild(input);
+}
+
+createBookSection('*Title:', 'input', 'title', 'Enter title');
+createBookSection('*Author:', 'input', 'author', 'Enter author');
+createBookSection('*Pages:', 'input', 'number', 'Enter pages');
+
+const formBtn = document.createElement('button');
+formBtn.type = 'submit';
+formBtn.id = 'btn';
+formBtn.name = 'btn';
+formBtn.textContent = 'Add Book';
+fieldset.appendChild(formBtn);
+
+form.appendChild(fieldset);
+
