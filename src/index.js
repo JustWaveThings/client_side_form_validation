d9435ddd77c3ec4152a46a1c64f6bfbefbd7f5b2
JustWaveThings <justwavethings@proton.me>
2023-03-15 12:52:47 -0400

    Remove IFFE, add start psuedocode to pageSwitcher function

--- src/index.js
+++ src/index.js
@@ -1,21 +1,30 @@
 import './style.css';
 import form from './signup-form';
-import submitSuccessContainer from './highFive';
-
+import successPage from './highFive';
+import test from './formValidation';
 
 // eslint-disable-next-line prefer-const
-let success = false;
+let submitSuccess = false;
 
-(function displayContainer(container){
+function pageSwitcher(){
+    // this will be set by  the sign up button actually working,  even though we aren't submiting a form... I have to figure that out  && the button on the success page to go back and submit another sign up. 
+
+    // when submit happens it should change the submitSuccess flag to true, and when the sign up again button is clicked, it should be set back to false.
+}
+
+function displayContainer(){
+    
     const body = document.querySelector('body');
-    if (container) {
-        body.appendChild(submitSuccessContainer);
+    if (submitSuccess) {
+        body.appendChild(successPage);
     }
-    if (!container) {
+    if (!submitSuccess) {
         body.appendChild(form);
     }
-   
-})(success)
+    const allInputs = document.querySelectorAll('input');
+    console.log(allInputs)
+}
+displayContainer()
 
 
 
